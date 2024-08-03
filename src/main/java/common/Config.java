15034bd857efa8459b2a774ee4772f3936e2ac6b
a-levitt <artemis.levitt@gmail.com>
2024-08-03 12:04:13 +0200

    Browser drivers initialization

---
+++ src/main/java/common/Config.java
@@ -0,0 +1,11 @@
+package common;
+
+public class Config {
+
+    /**
+     * Specify the browser for tests:
+     * CHROME
+     * MOZILLA
+     * */
+    public static final String BROWSER = "CHROME";
+}
